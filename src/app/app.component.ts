d2cc214b2afdaf5509e4d9abf312f13a8f2bfd24
Demmy <demolaabdulai@live.com>
2022-03-29 08:12:55 +0200

    first commit
    The first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  includeLetters = false; 
+  includeNumbers = false; 
+  includeSymbols = false; 
+  length = 0; 
+  password = ''; 
+  
+
+  onChangeUseLetters(){
+
+    this.includeLetters = !this.includeLetters; 
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers; 
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols; 
+  }
+
+  onChangeLength(value:string){
+    const parsedValue = parseInt(value); 
+    // to check if user enters a non number, it displays 0
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue
+    }
+
+    if(parsedValue < 0  || parsedValue > 20){
+    
+      alert ("Number out of range")
+    }
+
+    
+  }
+
+  onButtonClick(){
+    /* console.log("letter is " + this.includeLetters)
+    console.log("Number is " + this.includeNumbers)
+    console.log("symbol is " + this.includeSymbols)
+    this.password = "Demmys";  */
+
+    // generating Random password 
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!"#¤%&/()?@£$€';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+    let generatedPasword = ''; 
+    for (let i = 0; i < this.length; i ++) {
+      const index = Math.floor(Math.random()* validChars.length);
+      generatedPasword += validChars[index]
+    }
+    this.password = generatedPasword;
+
+
+    // console.log("valid chars are " + validChars)
+    console.log("valid chars are " + generatedPasword)
+  }
+
 }
+
